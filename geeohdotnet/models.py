9909b3872fbc0864ecf96a9fdec33f933152bb7c
Giovani1906 <4cc.giovani1906@gmail.com>
2024-03-28 19:12:26 +0100

    Initial move to SQL.

--- geeohdotnet/models.py
+++ geeohdotnet/models.py
@@ -1,6 +1,24 @@
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from markdown2 import markdown
 
 
-class User(AbstractUser):
-    def save(self, **kwargs):
-        pass
+class Article(models.Model):
+    id = models.IntegerField(primary_key=True, editable=False, null=False, unique=True)
+    date = models.DateField(null=False)
+    title = models.CharField(max_length=30, null=False, unique=True)
+    description = models.CharField(max_length=60, null=False)
+    content = models.TextField(null=False)
+
+    @property
+    def content_formatted(self):
+        extras = [
+            "break-on-newline",
+            "code-friendly",
+            "fenced-code-blocks",
+            "footnotes",
+            "smarty-pants",
+            "spoiler",
+            "strike",
+            "tables",
+        ]
+        return markdown(self.content, extras=extras)
--- geeohdotnet/settings.py
+++ geeohdotnet/settings.py
@@ -1,108 +1,73 @@
-"""
-Django settings for geeohdotnet project.
-"""
-import orjson as json
-
+import json
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open('settings.json', 'rb') as f:
+with open("settings.json", "rb") as f:
     settings = json.loads(f.read())
 
-SECRET_KEY = settings['secret_key']
+SECRET_KEY = settings["secret_key"]
 
-DEBUG = settings['debug']
+DEBUG = settings["debug"]
 
-ALLOWED_HOSTS = ['localhost', 'geeoh.net']
-
-# Application definition
+ALLOWED_HOSTS = ["localhost", "geeoh.net"]
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'axes',
-    'geeohdotnet',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "geeohdotnet",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'axes.middleware.AxesMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 if not DEBUG:
     SESSION_COOKIE_SECURE = True
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
-AUTH_USER_MODEL = 'geeohdotnet.User'
-
-AUTHENTICATION_BACKENDS = [
-    'axes.backends.AxesStandaloneBackend',
-    'geeohdotnet.backends.AuthBackend',
-]
-
-AXES_ENABLED = True
-AXES_FAILURE_LIMIT = 3
-AXES_LOCK_OUT_AT_FAILURE = True
-AXES_COOLOFF_TIME = 24
-AXES_HANDLER = 'axes.handlers.cache.AxesCacheHandler'
-AXES_LOCKOUT_TEMPLATE = 'ratelimit.html'
-AXES_VERBOSE = DEBUG
-AXES_IPWARE_PROXY_COUNT = 1
-AXES_IPWARE_META_PRECEDENCE_ORDER = [
-    'HTTP_X_REAL_IP',
-    'HTTP_X_FORWARDED_FOR',
-    'REMOTE_ADDR',
-]
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211' if not DEBUG else '192.168.1.128:11211',
-    }
-}
-
-ROOT_URLCONF = 'geeohdotnet.urls'
-
-LOGIN_URL = '/auth'
+ROOT_URLCONF = "geeohdotnet.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-# Database
+DATABASES = {
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+}
 
-DATABASES = {}
-
-# Internationalization
-
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/"
 DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600
